2d04f25bd64e2c91e5b075c57463bf8e9cb4299e
Elianabla <26eblankstein@gannacademy.org>
2025-05-09 10:06:01 -0400

    commit

--- src/App.java
+++ src/App.java
@@ -1,27 +1,47 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       
+
         // Step 1. Parse command-line arguments.
-    
+
         double Gravity = 6.67E-11;
         double distance;
 
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader("/Users/elianablankstein/Physics/src/hi.text"))) {
+            String line = reader.readLine();
+            String radius = reader.readLine();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("/Users/elianablankstein/Physics/src/hi.text"))) {
-        String line;
-        line = reader.readLine();
-        System.out.println(line);
+            int count = Integer.parseInt(line);
+            ArrayList<Planet> planets = new ArrayList<>();
+
+            for (int i = 0; i < count; i++) {
+                String hi = reader.readLine();
+                String[] parts = hi.split(" ");
+                double xpos = Double.parseDouble(parts[0]);
+                double ypos = Double.parseDouble(parts[1]);
+                double xvel = Double.parseDouble(parts[2]);
+                double yvel = Double.parseDouble(parts[3]);
+                double mass = Double.parseDouble(parts[4]);
+                String name = parts[5];
+                
+                planets.add(new Planet(xpos, ypos, xvel, yvel, mass, name));
+
+            }
+            System.out.println(line);
+            System.out.println(radius);
+
+          
+    
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-       
-        
- 
 
         // Step 2. Read the universe from standard input.
 
---
+++ src/Planet.java
@@ -0,0 +1,18 @@
+public class Planet {
+    double xpos;
+    double ypos;
+    double xvel;
+    double yvel;
+    double mass;
+    String name;
+
+
+    public Planet(double xpos, double ypos, double xvel, double yvel, double mass, String name){
+        this.xpos=xpos;
+        this.ypos = ypos;
+        this.xvel = xvel;
+        this.yvel = yvel;
+        this.mass = mass;
+        this.name = name;
+    }
+}
